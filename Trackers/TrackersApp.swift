866a6d23e8d21e498c10b764488acd31bc0a7774
Chaley Stallings <chaley.stallings@gmail.com>
2020-10-06 16:43:39 -0400

    Initial Commit

---
+++ Trackers/TrackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackersApp.swift
+//  Trackers
+//
+//  Created by Chaley Stallings  on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct TrackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
